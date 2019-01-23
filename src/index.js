a357268d9b6654c30f3fee3422b9b5678b11c3e0
cucmberslice <ywblackberry@gmail.com>
2019-01-22 21:29:39 -0500

    added mapbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const mapboxgl = require('mapbox-gl')
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2FuZ2R6aSIsImEiOiJjamdvNTB4YjkxZDQyMndybWhyenZxcXVuIn0.wCNqcCBSTcUiti67qxmzdw";
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    center: [-74.0009, 40.705], //Fullstack NY coordinates;
+    zoom: 12, //starting zoom
+    style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available
+})
