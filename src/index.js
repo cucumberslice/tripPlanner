f05adab2e6d4c34c4c8013773758034d983d252b
cucmberslice <ywblackberry@gmail.com>
2019-01-22 22:22:30 -0500

    can render mapbox on browser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const mapboxgl = require('mapbox-gl')
 
 mapboxgl.accessToken =
-  "pk.eyJ1Ijoid2FuZ2R6aSIsImEiOiJjamdvNTB4YjkxZDQyMndybWhyenZxcXVuIn0.wCNqcCBSTcUiti67qxmzdw";
+  "pk.eyJ1Ijoid2FuZ2R6aSIsImEiOiJjanI4a3F0dmMwN3B1NDRydnE5N2Fscm84In0.9Tdv4bYU0qFnuzGacVaknQ";
+
+
+
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    center: [-74.0009, 40.705], //Fullstack NY coordinates;
-    zoom: 12, //starting zoom
-    style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available
-})
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v11" // mapbox has lots of different map styles available.
+});
+
+
+module.exports = {map}
